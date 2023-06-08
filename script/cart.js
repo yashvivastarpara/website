feecd7b42691c43a31523395a24e497fbc4d59eb
yashvi <you@example.com>
2023-06-08 23:21:50 +0530

    cart

--- script/cart.js
+++ script/cart.js
@@ -10,17 +10,23 @@ let show = (data) => {
       let title = document.createElement("h3");
       title.setAttribute("class","titless");
       title.innerHTML = ele.title;
+      
+      let add = document.createElement("button");
+      add.innerHTML = ele.add
+      add.innerHTML = "+"
+      add.setAttribute("class","add")
+
       let price = document.createElement("p");
       price.innerHTML = ele.price;
       price.setAttribute("class","pricess");
-      let add = document.createElement("button");
-      add.innerHTML = ele.add
-      add.setAttribute("class","add")
+
       let remove = document.createElement("button");
       remove.innerHTML = ele.remove
+      remove.innerHTML = "-"
       remove.setAttribute("class","remove")
+
       let cartdiv = document.createElement("div");
-      cartdiv.append(img,title,price,add,remove)
+      cartdiv.append(img,title,add,price,remove)
       cartdiv.setAttribute("class", "cartdiv")
       document.getElementById("cart").append(cartdiv)
     });
